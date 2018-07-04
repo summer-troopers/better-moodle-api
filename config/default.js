749ccbe616ac4fc407899da8b71df6da7b3353d4
Alexandru Olaru <alxolr@gmail.com>
2018-07-04 18:11:05 +0300

    Fixed the configuration file

--- config/default.js
+++ config/default.js
@@ -2,11 +2,11 @@ module.exports = {
   mysql: process.env.MYSQL_URI || 'mysql://localhost:3306/moodle',
   sendgridApiKey: process.env.SENDGRID_API_KEY,
   port: process.env.PORT || 3000,
-  errorEmails: process.env.ERROR_EMAILS.split(';'),
+  errorEmails: process.env.ERROR_EMAILS,
 
   development: {
     username: process.env.SEQUELIZE_USER || 'root',
-    password: process.env.SEQUELIZE_PASSWORD,
+    password: process.env.SEQUELIZE_PASSWORD || 'pass',
     database: process.env.SEQUELIZE_DB || 'db',
     host: process.env.SEQUELIZE_HOST || 'localhost',
     dialect: process.env.SEQUELIZE_DIALECT || 'mysql',
