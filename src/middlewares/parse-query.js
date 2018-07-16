62e6401d569f78ee7de289eb29890b239b2c1b86
Maria Turuta <mariaturuta@gmail.com>
2018-07-16 18:10:02 +0300

    improved parse-query middleware

--- src/middlewares/parse-query.js
+++ src/middlewares/parse-query.js
@@ -1,10 +1,6 @@
 module.exports = function parseQueryParams(request, response, next) {
-  if (!request.query.limit) request.query.limit = '50';
-  request.query.limit = parseInt(request.query.limit, 10);
-
-  if (!request.query.offset) request.query.offset = '0';
-  request.query.offset = parseInt(request.query.offset, 10);
-
+  request.query.limit = parseInt(request.query.limit, 10) || 50;
+  request.query.offset = parseInt(request.query.offset, 10) || 0;
   if (!request.query.contains) request.query.contains = '';
 
   next();
