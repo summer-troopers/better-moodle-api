be1c6885ce97d57b394ae42130024146aa102948
Maria Turuta <mariaturuta@gmail.com>
2018-07-17 12:18:17 +0300

    fix eslint rules
    changed some sentence errors
    
    changed token
    
    fix error of undefined user
    
    removed modules
    
    package lock changed
    
    revert
    
    Fixed error in the seeders
    
    Fixed error in seeders, test, heash
    
    Fiexed bug in heash
    
    Fiexed bug in heash
    
    Fiexed bug in heash
    
    Fixed in comparePassword
    
    Fixed in comparePassword
    
    New fixes
    
    Fixet error
    
    Fixet error
    
    Fixed error in the seeders
    
    Fixed error in seeders, test, heash
    
    Fiexed bug in heash
    
    Fiexed bug in heash
    
    Fixed in comparePassword
    
    New fixes
    
    Fixet error

--- src/routes/authentication-route.js
+++ src/routes/authentication-route.js
@@ -3,19 +3,31 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const hashPassword = require('../helpers/hash/hash-factory')();
 
 module.exports = function createAuthenticationRoute(repository) {
   const router = express.Router();
 
   async function loginUser(request, response) {
-    const result = await repository.returnUser(request.body);
+    const result = await repository.getUser(request.body);
     if (!result) return response.sendStatus(404);
-    const token = jwt.sign(result, config.jwtconf.secret, config.jwtconf.time);
+    const token = jwt.sign({ role: result[0], user: result[1].id }, config.jwtconf.secret, config.jwtconf.time);
     return response.status(200).json({ token });
   }
 
+  async function comparePassword(request, response, next) {
+    const passwordDb = await repository.getUser(request.body);
+    try {
+      const compareResult = await hashPassword.compare(request.body.password, passwordDb[1].password);
+      if (compareResult) next();
+      else throw compareResult;
+    } catch (error) {
+      response.sendStatus(403);
+    }
+  }
+
   router.route('/')
-    .post(loginUser);
+    .post(comparePassword, loginUser);
 
   return router;
 };
--- test/src/middlewares/authorization-verifier.test.js
+++ test/src/middlewares/authorization-verifier.test.js
@@ -5,13 +5,12 @@ const config = require('config');
 const createAuthorizationVerifier = require('../../../src/middlewares/authorization-verifier');
 const roles = require('../../../src/helpers/constants/roles');
 
-
-const ENCRYPTED_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic3R1ZGVudCIsInVzZXIiOnsiaWQiOjEsImZpcnN0TmFtZSI6IlNoeWFubmUiLCJsYXN0TmFtZSI6IkhlcnpvZyIsInBhc3N3b3JkIjoibmF4b3h1cWkiLCJwaG9uZU51bWJlciI6IjUwMS0zODItMDc0OSIsImVtYWlsIjoiV3ltYW4uTmlrb2xhdXNAZ21haWwuY29tIiwiaWRHcm91cCI6OX0sImlhdCI6MTUzMTc0MjkwMSwiZXhwIjoxNTMxODI5MzAxfQ.pXQbjgRi4x_0xkT2ngdYtZnJ9Glz6uiXNeKB7obUFho';
+const ENCRYPTED_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidGVhY2hlciIsInVzZXIiOjEsImlhdCI6MTUzMTkxMDIxOSwiZXhwIjo4NjQxNTMxOTEwMjE5fQ.v0YZpn0beF5x7fqCy_0j0A1011PBhoz6ofKZVo2GPvo';
 
 const userRepository = {
   exists(id, role) {
     return new Promise((resolve, reject) => {
-      resolve(id === 1 && role === roles.STUDENT);
+      resolve(id === 1 && role === roles.TEACHER);
     });
   },
 };
