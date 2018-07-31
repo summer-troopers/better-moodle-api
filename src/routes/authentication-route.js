f1b0ae4aa18afb2da23690a58b8704d9cca57800
Vadim BRADULEAC <vbraduleac@pentalog.com>
2018-07-31 15:31:11 +0300

    object destructuring

--- src/routes/authentication-route.js
+++ src/routes/authentication-route.js
@@ -11,14 +11,13 @@ module.exports = function createAuthenticationRoute(repository) {
   async function loginUser(request, response) {
     const result = await repository.getUser(request.body);
     if (!result) return response.sendStatus(404);
-    const token = jwt.sign({ role: result[0], user: result[1].id }, config.jwtconf.secret, config.jwtconf.time);
-    const userData = {
+    const [token, userData] = [jwt.sign({ role: result[0], user: result[1].id }, config.jwtconf.secret, config.jwtconf.time), {
       id: result[1].id,
       firstName: result[1].firstName,
       lastName: result[1].lastName,
       phoneNumber: result[1].phoneNumber,
       email: result[1].email,
-    };
+    }];
     return response.status(200).json({ token, userData });
   }
 
