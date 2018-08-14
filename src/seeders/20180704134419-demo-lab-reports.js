eef13bad8aca9cadf206b769d6dad9b8f2572f5b
Maria Turuta <mariaturuta@gmail.com>
2018-08-14 15:05:34 +0300

    improved labtask and labreport seeders, fixed some other seeders

--- src/seeders/20180704134419-demo-lab-reports.js
+++ src/seeders/20180704134419-demo-lab-reports.js
@@ -2,18 +2,25 @@ const faker = require('faker');
 
 module.exports = {
   // eslint-disable-next-line no-unused-vars, no-use-before-define
-  up(queryInterface, Sequelize) { return queryInterface.bulkInsert('lab_reports', generateLabReports(), {}); },
+  up(queryInterface, Sequelize) { return queryInterface.bulkInsert('lab_reports', generate10LabReports(), {}); },
   // eslint-disable-next-line no-unused-vars
   down(queryInterface, Sequelize) { return queryInterface.bulkDelete('lab_reports', null, {}); },
 };
 
-function generateLabReports() {
-  const labTask = [];
-  labTask.push({
+function generate10LabReports() {
+  const labReports = [];
+  labReports.push({
     id: 1,
     student_id: '1',
     lab_task_id: '1',
     mongo_file_id: '1',
   });
-  return labTask;
+  for (let i = 0; i < 10; i += 1) {
+    labReports.push({
+      student_id: faker.random.number(40) + 1,
+      lab_task_id: faker.random.number(10) + 1,
+      mongo_file_id: faker.random.number(10) + 1,
+    });
+  }
+  return labReports;
 }
