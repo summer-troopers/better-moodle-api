d1d2f3f447d961400aa003c2ead27ae502b85af5
runtilov <40624285+runtilov@users.noreply.github.com>
2018-08-20 23:45:49 -0700

    Changed the default value for the 'limit' query param from '50' to '0' (#63)

--- src/middlewares/parse-query.js
+++ src/middlewares/parse-query.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function parseQueryParams(request, response, next) {
-  request.query.limit = parseInt(request.query.limit, 10) || 50;
+  request.query.limit = parseInt(request.query.limit, 10) || 0;
   request.query.offset = parseInt(request.query.offset, 10) || 0;
   request.query.contains = request.query.contains || '';
 
